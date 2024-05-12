a0aa56cd9a64888053ae1d5439e3b795b69dd3b5
Matundu <matundu.pro@gmail.com>
2024-05-12 20:37:36 +0200

    Validations inputs + errors msg + affichage du résultat

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # IMC-Calculator
 
-Ce projet est un calculateur d'Indice de Masse Corporelle (IMC) conçu pour aider les utilisateurs à évaluer leur poids corporel par rapport à leur taille.
+"**[IMC Calculator](imc-calculator.julesmatundu.com)**" est une application web simple conçue pour calculer l'Indice de Masse Corporelle (IMC) et fournir un feedback instantané sur la catégorie de poids selon les critères de l'OMS.
 
-L'IMC est une mesure simple qui permet de classer les adultes en fonction de leur poids en rapport avec leur taille et ainsi estimer leur état nutritionnel.
+## Fonctionnalités
+
+- **Calcul de l'IMC** : Permet aux utilisateurs de saisir leur poids en kilogrammes et leur taille en centimètres pour obtenir leur IMC.
+- **Catégorisation automatique** : Classifie automatiquement les résultats de l'IMC dans différentes catégories telles que maigreur, poids normal, surpoids, etc.
+- **Validation des entrées** : Vérifie que les entrées sont valides avec des messages d'erreur clairs si les valeurs saisies ne sont pas réalistes.
+
+## Technologies utilisées
+
+- **JavaScript** : Pour la logique de calcul et de validation.
+- **HTML/CSS** : Pour la structure et le style de l'application.
+
+## Installation
+
+Pour utiliser l'application en local avec Live Server:
+
+1. Clonez le dépôt sur votre machine locale.
+2. Ouvrez le dossier du projet avec un éditeur de code qui supporte Live Server (comme VSCode).
+3. Lancez l'application via Live Server.
+
+## Utilisation
+
+1. **Entrée des données** : Saisissez votre poids et votre taille dans les champs prévus.
+2. **Soumission** : Soumettez le formulaire pour voir votre IMC et la catégorie correspondante s'afficher.
+
+## Note
+
+Ce projet est destiné à démontrer mes compétences en développement web et n'est pas destiné à une utilisation commerciale.
+
+Il est disponible pour visualisation sur [IMC Calculator](imc-calculator.julesmatundu.com).
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const BMIData = [
-    { name: 'Maigreur', color: '#001F3F', range: [0, 18.5] },
-    { name: 'Poids normal', color: '#2ECC71', range: [18.5, 25] },
+    { name: 'Maigreur', color: '#A3D1FF', range: [0, 18.5] },
+    { name: 'Poids normal', color: '#65DC97', range: [18.5, 25] },
     { name: 'Surpoids', color: '#FFC0CB', range: [25, 30] },
-    { name: 'Obésité modérée', color: '#FFA500', range: [30, 35] },
-    { name: 'Obésité sévère', color: '#DC143C', range: [35, 40] },
-    { name: 'Obésité morbide', color: '#800080', range: [40] },
+    { name: 'Obésité modérée', color: '#FFBB3D', range: [30, 35] },
+    { name: 'Obésité sévère', color: '#F9BEC7', range: [35, 40] },
+    { name: 'Obésité morbide', color: '#FFB8FF', range: 40 },
 ]
 
 // IMC = poids en kg / taille² en m
@@ -12,22 +12,73 @@ const BMIData = [
 // Récupération des éléments HTML
 const form = document.querySelector('form')
 const inputs = document.querySelectorAll('input')
+const displayBMI = document.querySelector('.bmi-value')
+const result = document.querySelector('.result')
 
 // Event : Soummision du formulaire
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const bmi = calculateBMI(inputs)
-    console.log('IMC = ', bmi)
+    calculateBMI()
 })
 
 // Fonction pour calculer l'IMC
-function calculateBMI(inputs) {
+function calculateBMI() {
     const height = inputs[0].value
     const weight = inputs[1].value
 
-    console.log(height, weight)
+    const inputsAreValid = verifyInputs(weight, height)
+
+    if (!inputsAreValid) {
+        console.error(
+            'Veuillez saisir des valeurs valides pour le poids et la taille. \n\nLe poids doit être compris entre 1 kg et 300 kg, et la taille doit être comprise entre 1 cm et 250 cm.'
+        )
+        handleError()
+        return
+    }
 
     // prettier-ignore
-    return (weight / ((height/100) ** 2)).toFixed(2)
+    const BMI = (weight / ((height/100) ** 2)).toFixed(1)
+
+    showResult(BMI)
+}
+
+// Fonction de vérification des valeurs des inputs
+function verifyInputs(weight, height) {
+    // Vérifier si les deux inputs ne sont pas vides
+    if (weight === '' || height === '') {
+        return false // Au moins un input est vide
+    }
+
+    // Vérifier si les valeurs sont réalistes
+    if (weight <= 0 || height <= 0 || weight > 300 || height > 250) {
+        return false // Les valeurs ne reflètent pas la réalité
+    }
+
+    return true // Les deux inputs sont valides
+}
+
+function handleError() {
+    displayBMI.textContent = 'Oups !'
+    result.innerHTML =
+        'Veuillez saisir des valeurs valides pour le poids et la taille. <br /> Le poids doit être compris entre 1 kg et 300 kg, et la taille doit être comprise entre 1 cm et 250 cm.'
+
+    result.style.fontSize = '14px'
+    result.style.color = '#FFC0CB'
+    displayBMI.style.color = '#FFC0CB'
+}
+
+function showResult(BMI) {
+    const rank = BMIData.find((data) => {
+        if (BMI >= data.range[0] && BMI < data.range[1]) return data
+        else if (typeof data.range === 'number' && BMI >= data.range)
+            return data
+    })
+
+    displayBMI.textContent = BMI
+    displayBMI.style.color = '#f1f1f1'
+    displayBMI.style.color = rank.color
+
+    result.textContent = `Résultat : ${rank.name}`
+    result.style.fontSize = '20px'
 }
